d4dd2cd6dd4e25ddac7c75e3b6cd5d865a2dd7c8
mhazarabad <mhazarabad@gmail.com>
2026-03-02 11:27:00 +0100

    patch for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,10 @@ python manage.py migrate --noinput
 
 if [ "$DEBUG" = "True" ]; then
     echo "Starting Django development server..."
-    python manage.py runserver 0.0.0.0:8000
+    python manage.py runserver 0.0.0.0:8000 
 else
     echo "Starting Gunicorn..."
     exec gunicorn online_menu.wsgi:application \
         --bind 0.0.0.0:8000 \
         --workers 3
+fi
